3551f5969c3020df048c0759872758443420005a
jeod13 <92585056+jeod13@users.noreply.github.com>
2021-11-10 20:54:47 +0800

    Update unit.py

--- test/unit.py
+++ test/unit.py
@@ -1,17 +1,17 @@
 from flask import Flask
 import json
-from flask_pytest_example.handlers.routes import configure_routes
+#from flask_pytest_example.handlers.routes import configure_routes
 
 def test_login():
     #coonnect to flask
     app = Flask(__name__)
-    configure_routes(app)
+    #configure_routes(app)
     client = app.test_client()
     #test with the following url
-    url = '/login'
+    #url = '/login'
     #test data
     mock_request_data = {"username":"test","password":"test"}
     #send request and obtain response
-    response = client.post(url, data=json.dumps(mock_request_data))
+    response = client.post(http://localhost/login, data=json.dumps(mock_request_data))
     #determine the status code of the response is correct
     assert response.status_code == 200
