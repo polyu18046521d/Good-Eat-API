b08d0a5f107c9fdee234adb9e161060fe54e0291
jeod13 <92585056+jeod13@users.noreply.github.com>
2021-11-11 21:11:22 +0800

    Update unit.py

--- test/unit.py
+++ test/unit.py
@@ -31,6 +31,10 @@ class TestClass:
             self.url_helper("/login"), json={"username":"test","password":"test"}
         )
         json_data = res0.json()
+        #token = json.dumps(json_data)
+       # token_helper = json_data["access_token"]
+        #username = request.json.get("username", None)
+        #token = res0.json.post("access_token",None)
         
         data = json.dumps(json_data)
         count0 =0
@@ -40,15 +44,14 @@ class TestClass:
         count1 = 0
         while data[count1]!='}':
 	        count1=count1+1
-		
+
         token=data[count0+3:count1-1]
         whole_token="Bearer " + token
         print(whole_token)
-	#token_helper = json.loads(json_data)
+        
+        #token_helper = json.loads(json_data)
         #token = token_helper["access_token"]
-	#headers = {'Authorization': 'Bearer {}'.format(whole_token)}
-	headers = {'Authorization': 'Bearer {}'.format(whole_tocken)}
-	#headers = {'Authorization': whole_token}
+        headers = {'Authorization': whole_token}
         #headers = {'Authorization':"Bearer" + token_helper}
         res = requests.get(self.url_helper("/eats/00001"),headers=headers)
         #json_data = res.json()
