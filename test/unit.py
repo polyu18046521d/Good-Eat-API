3a38751511747ea0e7f2a4dc7314418c2d33808d
polyu18046521d <18046521d@connect.polyu.hk>
2021-11-11 21:59:10 +0800

    Updated testcases

--- test/unit.py
+++ test/unit.py
@@ -1,12 +1,10 @@
-#from flask import Flask
-import json
-#from flask_pytest_example.handlers.routes import configure_routes
 import requests
+
 
 class TestClass:
     def url_helper(self, path):
         return "http://localhost" + path
-    
+
     def parse(self, obj):
         if isinstance(obj, dict):
             return sorted((k, self.parse(v)) for k, v in obj.items())
@@ -14,130 +12,101 @@ class TestClass:
             return sorted(self.parse(x) for x in obj)
         else:
             return obj
-    
-    def token_helper(self):
-        #tbc
-        return token
-    
-    def test_login(self):
+
+    def get_jwt_token(self):
         res = requests.post(
-            self.url_helper("/login"), json={"username":"test","password":"test"}
+            self.url_helper("/login"), json={"username": "test", "password": "test"}
+        )
+        return res.json().get("access_token")
+
+    def auth_header(self):
+        token = self.get_jwt_token()
+        return {"Authorization": f"Bearer {token}"}
+
+    def test_valid_login(self):
+        res = requests.post(
+            self.url_helper("/login"), json={"username": "test", "password": "test"}
         )
         json_data = res.json()
         assert res.status_code == 200
-    #Restaurant service    
-    def test_eats_storeid(self):
-        res0 = requests.post(
-            self.url_helper("/login"), json={"username":"test","password":"test"}
-        )
-        json_data = res0.json()
-        #token = json.dumps(json_data)
-       # token_helper = json_data["access_token"]
-        #username = request.json.get("username", None)
-        #token = res0.json.post("access_token",None)
-        
-        data = json.dumps(json_data)
-        count0 =0
-        while data[count0]!=':':
-	        count0=count0+1
+        assert res.headers["content-type"] == "application/json"
+        assert "access_token" in json_data
 
-        count1 = 0
-        while data[count1]!='}':
-	        count1=count1+1
-
-        token=data[count0+3:count1-1]
-        whole_token="Bearer " + token
-        print(whole_token)
-        
-        #token_helper = json.loads(json_data)
-        #token = token_helper["access_token"]
-        headers = {'Authorization': whole_token}
-        #headers = {'Authorization':"Bearer" + token_helper}
-        res = requests.get(self.url_helper("/eats/00001"),headers=headers)
-        #json_data = res.json()
-        assert res.status_code == 200
-		   
-    def test_eats_storeid_menu(self):
-        res0 = requests.post(
-            self.url_helper("/login"), json={"username":"test","password":"test"}
-        )
-        json_data = res0.json()
-                
-        data = json.dumps(json_data)
-        count0 =0
-        while data[count0]!=':':
-	        count0=count0+1
-
-        count1 = 0
-        while data[count1]!='}':
-	        count1=count1+1
-
-        token=data[count0+3:count1-1]
-        
-        headers = {'Authorization':"Bearer " + token,'Content-Type': 'application/json'}
+    def test_invalid_login(self):
         res = requests.post(
-		self.url_helper("/eats/00001/menu"), json={"menu_id": "D","name": "Set Lunch D","price": "150"}
-	    )   
-        assert res.status_code == 200
-	# Order service	   
-    def test_eats_order_orderid(self):
-        res0 = requests.post(
-            self.url_helper("/login"), json={"username":"test","password":"test"}
+            self.url_helper("/login"), json={"username": "test", "password": "123"}
         )
-        json_data = res0.json()
-                
-        data = json.dumps(json_data)
-        count0 =0
-        while data[count0]!=':':
-	        count0=count0+1
+        json_data = res.json()
+        assert res.status_code == 401
+        assert res.headers["content-type"] == "application/json"
+        assert self.parse(json_data) == self.parse({"msg": "Bad username or password"})
 
-        count1 = 0
-        while data[count1]!='}':
-	        count1=count1+1
-
-        token=data[count0+3:count1-1]
-        
-        headers = {'Authorization':"Bearer " + token}
+    # Restaurant service
+    def test_eats_storeId(self):
         res = requests.get(
-		    self.url_helper("/eats/order/000011")
-	    )   
+            self.url_helper("/eats/00001"),
+            headers=self.auth_header(),
+        )
+        json_data = res.json()
         assert res.status_code == 200
+        assert res.headers["content-type"] == "application/json"
+        assert self.parse(json_data) == self.parse(
+            {
+                "menus": [
+                    {"menu_id": "A", "name": "Drink A", "price": 30.0},
+                    {"menu_id": "B", "name": "Drink B", "price": 36.0},
+                ],
+                "name": "store-00001",
+                "status": "ONLINE",
+                "store_id": "00001",
+            }
+        )
+
+    def test_eats_storeId_menu(self):
+        res = requests.post(
+            self.url_helper("/eats/00001/menu"),
+            json={"menu_id": "D", "name": "Set Lunch D", "price": "150"},
+            headers=self.auth_header(),
+        )
+        assert res.status_code == 204
+        assert res.headers["content-type"] == "application/json"
+        assert res.content == b""
+
+    # Order service
+    def test_eats_order_orderId(self):
+        res = requests.get(
+            self.url_helper("/eats/order/000011"),
+            headers=self.auth_header(),
+        )
+        json_data = res.json()
+        assert res.status_code == 200
+        assert res.headers["content-type"] == "application/json"
+        assert self.parse(json_data) == self.parse(
+            {
+                "details": [{"count": 1.0, "menu_id": "A"}],
+                "order_id": "000011",
+                "status": "PENDING",
+                "store_id": "00001",
+            }
+        )
 
     def test_eats_order(self):
-        res0 = requests.post(
-            self.url_helper("/login"), json={"username":"test","password":"test"}
+        res = requests.post(
+            self.url_helper("/eats/order"),
+            json={"store_id": "00001", "details": [{"menu_id": "A", "count": 1}]},
+            headers=self.auth_header(),
         )
-        json_data = res0.json()
-                
-        data = json.dumps(json_data)
-        count0 =0
-        while data[count0]!=':':
-	        count0=count0+1
+        json_data = res.json()
+        assert res.status_code == 201
+        assert res.headers["content-type"] == "application/json"
+        assert "order_id" in json_data
 
-        count1 = 0
-        while data[count1]!='}':
-	        count1=count1+1
-
-        token=data[count0+3:count1-1]
-        
-        headers = {'Authorization':"Bearer " + token,'Content-Type': 'application/json'}
+    def test_eats_order_orderId_status(self):
         res = requests.post(
-		self.url_helper("/eats/order"), json={
-                                                "store_id": "00001",
-                                                "details": [
-                                                    {
-                                                        "menu_id": "A",
-                                                        "count": 1
-                                                    }
-                                                ]             
-                                             }
-	    )   
-        assert res.status_code == 200        
-
-    def test_eats_order_orderid_status(self):
-              
-        headers = {'Content-Type': 'application/json'}
-        res = requests.post(
-		self.url_helper("/eats/order/000011/status"), json={"status": "ACCEPTED"}
-	    )   
-        assert res.status_code == 200
+            self.url_helper("/eats/order/000011/status"),
+            json={"status": "ACCEPTED"},
+            headers=self.auth_header(),
+        )
+        assert res.status_code == 204
+        assert res.headers["content-type"] == "application/json"
+        assert res.content == b""
