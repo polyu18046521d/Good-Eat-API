433ad4a71cd5cab0bb27e3c3c9d6968b21737f32
jeod13 <92585056+jeod13@users.noreply.github.com>
2021-11-11 10:50:00 +0800

    Update unit.py

--- test/unit.py
+++ test/unit.py
@@ -55,7 +55,7 @@ class TestClass:
         
         #token_helper = json.loads(json_data)
         #token = token_helper["access_token"]
-        headers = {'Authorization':"Bearer" + token}
+        headers = {'Authorization':"Bearer " + token}
         #headers = {'Authorization':"Bearer" + token_helper}
         res = requests.get(self.url_helper("/eats/00001"),headers=headers)
         #json_data = res.json()
